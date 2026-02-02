77226a107a62789d25f24021cdcb7c56bdc60b1e
LincoHd <47218525+LincoHd@users.noreply.github.com>
2026-02-02 20:08:45 +0100

    Cerus Wall and Orb
    start of both wall and orb

--- src/skill_ids.h
+++ src/skill_ids.h
@@ -124,6 +124,7 @@ const uint32_t MECHANIC_QADIM_TELEPORT = 52281;//Swap
 const uint32_t MECHANIC_CERUS_ENVIOUS_GAZE_A = 72200; //Wall Single or Double?
 const uint32_t MECHANIC_CERUS_ENVIOUS_GAZE_B = 70790; //Wall Single or Double?
 const uint32_t MECHANIC_CERUS_ENVIOUS_GAZE_C = 70538; //Wall Single or Double?
+const uint32_t MECHANIC_CERUS_ENVIOUS_GAZE_D = 69814; //Wall Single or Double?
 const uint32_t MECHANIC_CERUS_Insatiable = 70253; //Orb Collected Buff
 
 //Greer
