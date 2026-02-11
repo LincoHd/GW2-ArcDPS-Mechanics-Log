ea125e75f958a1b8b5168d28f8e9890c094bd81c
LincoHd <47218525+LincoHd@users.noreply.github.com>
2026-02-11 18:46:03 +0100

    Decima Arrow and Aoe added
    Added detection for getting hit twice from Aoe or Arrow. 1 time hit is normal and intentional mechanic but getting hit 2 times often is downstate.

--- src/mechanics.h
+++ src/mechanics.h
@@ -125,6 +125,10 @@ struct DeimosOil
 	uint64_t last_touch_time = 0;
 };
 
+bool requirementDecimaExposedFluxance(const Mechanic & current_mechanic, cbtevent* ev, ag* ag_src, ag* ag_dst, Player * player_src, Player * player_dst, Player * current_player);
+
+bool requirementDecimaExposedChorusOfThunder(const Mechanic& current_mechanic, cbtevent* ev, ag* ag_src, ag* ag_dst, Player* player_src, Player* player_dst, Player* current_player);
+
 bool requirementDeimosOil(const Mechanic & current_mechanic, cbtevent* ev, ag* ag_src, ag* ag_dst, Player * player_src, Player * player_dst, Player * current_player);
 
 bool requirementOnSelf(const Mechanic & current_mechanic, cbtevent* ev, ag* ag_src, ag* ag_dst, Player * player_src, Player * player_dst, Player * current_player);
